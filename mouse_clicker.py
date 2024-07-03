f2d79f3a6daf9d374730209943cfb302df5839b0
Adrian Evangelista <adriane.macer@gmail.com>
2024-07-04 02:38:03 +0800

    other button click

--- mouse_clicker.py
+++ mouse_clicker.py
@@ -4,23 +4,13 @@ import time
 import pyautogui
 
 
-def start() :
+def start():
     controller = mouse.Controller()
     # time.sleep(2)
     # location = controller.position
     # print(f"Location {location[0]}, {location[1]}")
-    # controller.move( 100, 20, )
-    # location = controller.position
-    # print(f"Location {location[0]}, {location[1]}")
-    # time.sleep(2)
-    # controller.move( 10, 200, )
-    # location = controller.position
-    # print(f"Location {location[0]}, {location[1]}")
-    # time.sleep(2)
-    # controller.move( 150, 60, )
-    # location = controller.position
-    # print(f"Location {location[0]}, {location[1]}")
 
+    #  main battle
     while 1:
         time.sleep(2)
         # Battle Button
@@ -34,10 +24,30 @@ def start() :
         # Next button
         pyautogui.click(1766, 719)
 
+    # Head Hunt
+    # while 1:
+    #     time.sleep(2)
+    #     # ######################
+    #     # Hard Button
+    #     # pyautogui.click(1838, 820)
+    #     # ################################
+    #
+    #     # ######################
+    #     # Dessert Button
+    #     pyautogui.click(1823, 626)
+    #     # ################################
+    #
+    #     time.sleep(2)
+    #     # Battle Button
+    #     pyautogui.click(1717, 874)
+    #
+    #     time.sleep(2)
+    #     # Next button
+    #     pyautogui.click(1776, 660)
+
 
 if __name__ == "__main__":
     start()
-
 
 # battle button
 # 1717, 874
@@ -47,3 +57,13 @@ if __name__ == "__main__":
 
 # Next button
 # 1766, 719
+
+
+# Head Hunt hard attack button
+# 1838, 820
+
+# Head Hunt dessert attack button
+# 1823, 626
+
+# Fight button
+# 1717, 874
